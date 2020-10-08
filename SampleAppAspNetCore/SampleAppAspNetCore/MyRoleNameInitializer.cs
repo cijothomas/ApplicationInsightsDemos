989816aebc53917985449944abc68ced6a87764a
Cijo Thomas <cithomas@microsoft.com>
2020-10-07 17:32:21 -0700

    RoleName override with Initializer

---
+++ SampleAppAspNetCore/SampleAppAspNetCore/MyRoleNameInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace SampleAppAspNetCore
+{
+    internal class MyRoleNameInitializer : ITelemetryInitializer
+    {
+        private readonly string roleName;
+        public MyRoleNameInitializer(string roleName)
+        {
+            this.roleName = roleName;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = this.roleName;
+        }
+    }
+}
--- SampleAppAspNetCore/SampleAppAspNetCore/Startup.cs
+++ SampleAppAspNetCore/SampleAppAspNetCore/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +27,7 @@ namespace SampleAppAspNetCore
         {
             services.AddControllers();
             services.AddApplicationInsightsTelemetry("instrumentationkeygoeshere");
+            services.AddSingleton<ITelemetryInitializer>(new MyRoleNameInitializer("CustomRoleName"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
