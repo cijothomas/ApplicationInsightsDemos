876508ae0412b3b2b96fc45378a7478ab788f435
Cijo Thomas <cithomas@microsoft.com>
2020-10-07 17:29:59 -0700

    Enable ApplicationInsights in code.

--- SampleAppAspNetCore/SampleAppAspNetCore/Startup.cs
+++ SampleAppAspNetCore/SampleAppAspNetCore/Startup.cs
@@ -25,6 +25,7 @@ namespace SampleAppAspNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddApplicationInsightsTelemetry("instrumentationkeygoeshere");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
